6fa53bcbf92b37d41073df369be164dce1b8396d
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-02-20 00:25:26 +0530

    try using futures

--- cpp/src/arrow/dataset/scanner.cc
+++ cpp/src/arrow/dataset/scanner.cc
@@ -221,7 +221,7 @@ Result<std::shared_ptr<Table>> Scanner::ToTable() {
     boost::async([&] () {
       auto batch_it = scan_task->Execute().ValueOrDie();
       return batch_it;
-    }).then([&] (std::future<RecordBatchIterator> f) {
+    }).then([&] (boost::future<RecordBatchIterator> f) {
       auto batch_it = f.get();
       auto local = batch_it.ToVector().ValueOrDie();
       state->Emplace(std::move(local), id);
